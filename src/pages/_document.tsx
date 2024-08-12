c5dfc948ea92428480e521919196a7b245d568a4
Jemima Luz <contatojemima@gmail.com>
2024-08-12 15:30:32 +0000

    setup: eslint
    - run npm i @rocketseat/eslint-config@1.1.3
    - run npm run lint
    - run npm run lint -- --fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,28 @@
-import { getCssText } from "@ignite-ui/react";
-import { Head, Html, Main, NextScript } from "next/document";
-import React from 'react';
+import { getCssText } from '@ignite-ui/react'
+import { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
 
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
 
         {/* enable css with SSR (works with disabled js) */}
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }}/>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
       <body>
         <Main />
