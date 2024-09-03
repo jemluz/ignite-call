884bd03ebc2597e8f0335f4674ad0de7755aac96
Jemima Luz <contatojemima@gmail.com>
2024-09-03 13:25:21 +0000

    feat: add calendar permission verification
    - setup scope permissions
    - add calendar permission verify

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -6,7 +6,33 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          // setup which pemissions will be asked for
+          // userinfo.email
+          // userinfo.profile
+          // calendar
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+
+  // callbacks are strategic functions that will be called at specific moments
+  callbacks: {
+    async signIn({ account }) {
+      // that function could return true or false, where true means ok, and false means not ok
+      // the string`s return work as a false
+      if (
+        // if not has the calendar pemission, return an error
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions';
+      }
+
+      return true;
+    },
+  },
 };
 export default NextAuth(authOptions);
